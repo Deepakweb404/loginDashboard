4561ffcc59b51a3b74603bbc8e6165290285c71e
Deepak Ahirwar <deepak.ahirwar@salt-tech.com>
2022-12-26 18:13:08 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { SigninSignupComponent } from './signin-signup/signin-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SigninSignupComponent },
+  { path: 'sigin-signup', component: SigninSignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SigninSignupComponent } from './signin-signup/signin-signup.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SigninSignupComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
